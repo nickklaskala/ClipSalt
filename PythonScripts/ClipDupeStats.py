e1a4ab095a4f5bb4505fe207c1426da451420457
Nick Klaskala <Nick.klaskala@trialcard.com>
2021-09-20 08:13:39 -0400

    Release 1.0.2

--- PythonScripts/ClipDupeStats.py
+++ PythonScripts/ClipDupeStats.py
@@ -1,9 +1,18 @@
 import pyperclip as pc
 x=pc.paste().splitlines()
-xi=[i.lower() for i in x]
+xi=sorted([i.lower() for i in x])
+
+
+dupes=[]+[x[0]]
+for i in range(1,len(xi)):
+	if xi[i]==xi[i-1]:
+		dupes.append(xi[i])
+dupes=list(set(dupes))
+
+
 d=[]
 di=[]
-for i in x:
+for i in dupes:
 	if i.lower() not in di:# and xi.count(i.lower())>1:
 		d.append(('       '+str(xi.count(i.lower())))[-7:]+'	'+i)
 		di.append(i.lower())
--- PythonScripts/ClipDupes.py
+++ PythonScripts/ClipDupes.py
@@ -1,16 +1,24 @@
 import pyperclip as pc
 x=pc.paste().splitlines()
-xi=[i.lower() for i in x]
+xi=sorted([i.lower() for i in x])
+
+
+dupes=[]+[x[0]]
+for i in range(1,len(xi)):
+	if xi[i]==xi[i-1]:
+		dupes.append(xi[i])
+dupes=list(set(dupes))
+
+
 d=[]
 di=[]
-for i in x:
+for i in dupes:
 	if i.lower() not in di and xi.count(i.lower())>1:
 		d.append(i)
 		di.append(i.lower())
-		x.remove(i)
+		# x.remove(i)
 		xi.remove(i.lower())
 pc.copy('\n'.join(set(d)))
-
 
 
 
